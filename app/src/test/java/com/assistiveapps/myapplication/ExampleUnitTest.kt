6efe18adf4377e9dcdeea7444eb5433bab9bf647
Robin Kamboj <dev.robinkamboj@gmail.com>
2019-04-01 14:25:55 +0530

    sample unit test

--- app/src/test/java/com/assistiveapps/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/assistiveapps/myapplication/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.assistiveapps.myapplication
 
+import com.assistiveapps.myapplication.util.StringUtils
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,8 +11,14 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun date_isCorrect() {
+        assertEquals("2019-04-01", StringUtils.getDateFromString("2019-04-01T05:42:00Z"))
+    }
+
+    @Test
+    fun time_isCorrect() {
+        assertEquals("05:42", StringUtils.getTimeFromString("2019-04-01T05:42:00Z"))
     }
 }
